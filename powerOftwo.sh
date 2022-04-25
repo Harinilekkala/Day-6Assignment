39cfa4f249485b57b91a1f716e72444926d474f6
Harinilekkala <harini.lekkala1997@gmail.com>
2022-04-25 17:46:08 +0530

    added all the files

---
+++ powerOftwo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+echo "---------------------------------------------------------------"
+echo "Write a program that takes a command-line argument n and prints a table of the
+
+              powers of 2 that are less than or equal to 2^n"
+
+echo "-----------------------------------------------------------------"
+for i in {0..10};
+do
+        echo $((2**i));
+done
